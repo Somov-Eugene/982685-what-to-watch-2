7328de4d2442f389c05402f9c099deb197d82b24
Somov Eugene <somov.eugene.2012@gmail.com>
2019-10-29 01:27:52 +0300

    обновлен снепшот-тест компонента SmallMovieCard

--- src/components/small-movie-card/small-movie-card.test.js
+++ src/components/small-movie-card/small-movie-card.test.js
@@ -1,16 +1,21 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import SmallMovieCard from "./small-movie-card";
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import films from '../../mocks/films';
+import SmallMovieCard from './small-movie-card';
 
 describe(`Проверка <SmallMovieCard>`, () => {
   it(`компонент <SmallMovieCard> корректно отрисован`, () => {
-    const movie = {name: `Fantastic Beasts`};
-    const handlerLinkClick = jest.fn();
+    const movie = films[0];
+    const handleLinkClick = jest.fn();
+    const handleMouseEnter = jest.fn();
 
     const tree = renderer
       .create(<SmallMovieCard
         movie = {movie}
-        onLinkClick = {handlerLinkClick}
+        onLinkClick = {handleLinkClick}
+        onMouseEnter = {handleMouseEnter}
+        key = {movie.id}
       />)
       .toJSON();
 
