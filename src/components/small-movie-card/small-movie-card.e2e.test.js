b58d6267c421f5a75a2166952e08bbbb39b6c266
Somov Eugene <somov.eugene.2012@gmail.com>
2019-10-29 08:08:25 +0300

    e2e-тест проверяет, что при наведении на карточку с фильмом в обработчик попадает информация о фильме

--- src/components/small-movie-card/small-movie-card.e2e.test.js
+++ src/components/small-movie-card/small-movie-card.e2e.test.js
@@ -1,25 +1,46 @@
-import React from "react";
-import Enzyme, {shallow} from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import SmallMovieCard from "./small-movie-card";
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import films from '../../mocks/films';
+import SmallMovieCard from './small-movie-card';
 
 Enzyme.configure({adapter: new Adapter()});
 
 describe(`Проверка <SmallMovieCard>`, () => {
   it(`click по заголовку с названием фильма`, () => {
-    const movie = {
-      name: `Fantastic Beasts`
-    };
-    const handlerLinkClick = jest.fn();
+    const movie = films[0];
+    const handleLinkClick = jest.fn();
+    const handleMouseEnter = jest.fn();
 
     const movieCard = shallow(<SmallMovieCard
       movie = {movie}
-      onLinkClick = {handlerLinkClick}
+      onLinkClick = {handleLinkClick}
+      onMouseEnter = {handleMouseEnter}
+      key = {movie.id}
     />);
 
     const link = movieCard.find(`.small-movie-card__link`);
     link.simulate(`click`);
 
-    expect(handlerLinkClick).toHaveBeenCalledTimes(1);
+    expect(handleLinkClick).toHaveBeenCalledTimes(1);
+  });
+
+  it(`наведение курсора мыши на карточку с фильмом`, () => {
+    const movie = films[0];
+    const handleLinkClick = jest.fn();
+    const handleMouseEnter = jest.fn();
+
+    const smallCard = shallow(<SmallMovieCard
+      movie = {movie}
+      onLinkClick = {handleLinkClick}
+      onMouseEnter = {handleMouseEnter(movie)}
+      key = {movie.id}
+    />);
+
+    smallCard.find(`.small-movie-card`).simulate(`mouseenter`);
+
+    expect(handleMouseEnter).toHaveBeenCalledTimes(1);
+    expect(handleMouseEnter).toHaveBeenCalledWith(movie);
   });
 });
