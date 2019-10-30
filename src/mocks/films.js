566bd3eae51db5e1bd90ffdff00a16a0f5dd18d1
Somov Eugene <somov.eugene.2012@gmail.com>
2019-10-30 23:55:52 +0300

    в файл с моками /src/mocks/films.js добавлены ссылки на превью

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -2,27 +2,32 @@ const films = [
   {
     id: 1,
     name: `Fantastic Beasts: The Crimes of Grindelwald`,
-    poster: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+    poster: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    preview: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
   },
   {
     id: 2,
     name: `Bohemian Rhapsody`,
-    poster: `img/bohemian-rhapsody.jpg`
+    poster: `img/bohemian-rhapsody.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
   },
   {
     id: 3,
     name: `Macbeth`,
-    poster: `img/macbeth.jpg`
+    poster: `img/macbeth.jpg`,
+    preview: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
   },
   {
     id: 4,
     name: `Aviator`,
-    poster: `img/aviator.jpg`
+    poster: `img/aviator.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
   },
   {
     id: 5,
     name: `We need to talk about Kevin`,
-    poster: `img/we-need-to-talk-about-kevin.jpg`
+    poster: `img/we-need-to-talk-about-kevin.jpg`,
+    preview: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
   }
 ];
 
