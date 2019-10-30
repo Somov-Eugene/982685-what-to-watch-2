f1b3c6489aa18ac2ee54c6568cc72168d708d192
Somov Eugene <somov.eugene.2012@gmail.com>
2019-10-30 23:43:30 +0300

    оптимизация и исправление e2e-теста

--- src/components/small-movie-card/small-movie-card.e2e.test.js
+++ src/components/small-movie-card/small-movie-card.e2e.test.js
@@ -8,38 +8,24 @@ import SmallMovieCard from './small-movie-card';
 Enzyme.configure({adapter: new Adapter()});
 
 describe(`Проверка <SmallMovieCard>`, () => {
+  const movie = films[0];
+  const handleLinkClick = jest.fn();
+  const handleMouseEnter = jest.fn();
+
+  const movieCard = shallow(<SmallMovieCard
+    movie = {movie}
+    onLinkClick = {handleLinkClick}
+    onMouseEnter = {handleMouseEnter}
+    key = {movie.id}
+  />);
+
   it(`click по заголовку с названием фильма`, () => {
-    const movie = films[0];
-    const handleLinkClick = jest.fn();
-    const handleMouseEnter = jest.fn();
-
-    const movieCard = shallow(<SmallMovieCard
-      movie = {movie}
-      onLinkClick = {handleLinkClick}
-      onMouseEnter = {handleMouseEnter}
-      key = {movie.id}
-    />);
-
-    const link = movieCard.find(`.small-movie-card__link`);
-    link.simulate(`click`);
-
+    movieCard.find(`.small-movie-card__link`).simulate(`click`);
     expect(handleLinkClick).toHaveBeenCalledTimes(1);
   });
 
   it(`наведение курсора мыши на карточку с фильмом`, () => {
-    const movie = films[0];
-    const handleLinkClick = jest.fn();
-    const handleMouseEnter = jest.fn();
-
-    const smallCard = shallow(<SmallMovieCard
-      movie = {movie}
-      onLinkClick = {handleLinkClick}
-      onMouseEnter = {handleMouseEnter(movie)}
-      key = {movie.id}
-    />);
-
-    smallCard.find(`.small-movie-card`).simulate(`mouseenter`);
-
+    movieCard.find(`.small-movie-card`).simulate(`mouseenter`, movie);
     expect(handleMouseEnter).toHaveBeenCalledTimes(1);
     expect(handleMouseEnter).toHaveBeenCalledWith(movie);
   });
