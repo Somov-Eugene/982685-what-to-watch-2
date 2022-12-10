c460bab26d71eb60f2730f4354899fbf9c728476
Somov Eugene <somov.eugene.2012@gmail.com>
2022-12-10 23:27:32 +0300

    С помощью созданных компонентов описывает все маршруты в приложении.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from '../../pages/main-page/main-page';
+import SignInPage from '../../pages/sign-in-page/sign-in-page';
+import PrivateRoute from '../private-route/private-route';
+import MyListPage from '../../pages/my-list-page/my-list-page';
+import MoviePage from '../../pages/movie-page/movie-page';
+import AddReviewPage from '../../pages/add-review-page/add-review-page';
+import PlayerPage from '../../pages/player-page/player-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
 import { promoFilmType } from '../../types/films';
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 type AppProps = {
   promo: promoFilmType;
@@ -7,7 +16,42 @@ type AppProps = {
 
 function App({ promo }: AppProps): JSX.Element {
   return (
-    <MainPage promo={promo} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage promo={promo} />}
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignInPage />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <MyListPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<MoviePage />}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReviewPage />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerPage />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
