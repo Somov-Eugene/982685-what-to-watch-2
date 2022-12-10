bd446dcfa7910638c7ae66f01d96af57a957380e
Somov Eugene <somov.eugene.2012@gmail.com>
2022-12-10 21:13:28 +0300

    Обновляет index.tsx - отрисовывает компонент App и передаёт ему через props данные, которые нужны в компоненте главной страницы приложения.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+import { promoFilmType } from './types/films';
+
+const Promo: promoFilmType = {
+  id: 10,
+  name: 'The Grand Budapest Hotel',
+  posterImage: 'img/the-grand-budapest-hotel-poster.jpg',
+  previewImage: 'img/the-grand-budapest-hotel-poster.jpg',
+  backgroundImage: 'img/the-grand-budapest-hotel-poster.jpg',
+  backgroundColor: '#AD9F8B',
+  videoLink: 'https://11.react.pages.academy/static/film/video/bubbles.mp4',
+  previewVideoLink: 'https://11.react.pages.academy/static/film/video/traffic.mp4',
+  description: 'A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel\'s glorious years under an exceptional concierge.',
+  rating: 8.1,
+  scoresCount: 808357,
+  director: 'Wes Anderson',
+  starring: [
+    'Ralph Fiennes',
+    'Mathieu Amalric',
+    'Bill Murray',
+  ],
+  runTime: 100,
+  genre: 'Drama',
+  released: 2014,
+  isFavorite: false
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -8,6 +33,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App promo={Promo} />
   </React.StrictMode>,
 );
