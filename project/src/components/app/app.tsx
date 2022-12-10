a61e3f8c34af87affbc6b7a8f0c1a27899c6d3ef
Somov Eugene <somov.eugene.2012@gmail.com>
2022-12-10 21:09:00 +0300

    Обновляет компонент: App должен отрисовывать главную страницу.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,14 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+import { promoFilmType } from '../../types/films';
+
+type AppProps = {
+  promo: promoFilmType;
+}
+
+function App({ promo }: AppProps): JSX.Element {
+  return (
+    <MainPage promo={promo} />
+  );
 }
 
 export default App;
