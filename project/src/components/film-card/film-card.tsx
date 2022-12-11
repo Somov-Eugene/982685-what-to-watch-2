bf437e01005c375a2dd9fb1fea60eebaf6c0cc37
Somov Eugene <somov.eugene.2012@gmail.com>
2022-12-11 22:50:21 +0300

    Обновляет компонент «Карточка фильма».

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -4,13 +4,19 @@ import { FilmType } from '../../types/films';
 
 type FilmCardProps = {
   film: FilmType;
+  handleMouseEnter: () => void;
+  handleMouseLeave: () => void;
 }
 
-function FilmCard({ film }: FilmCardProps): JSX.Element {
+function FilmCard({ film, handleMouseEnter, handleMouseLeave }: FilmCardProps): JSX.Element {
   const {id, name, posterImage} = film;
 
   return (
-    <article className="small-film-card catalog__films-card">
+    <article
+      className="small-film-card catalog__films-card"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <div className="small-film-card__image">
         <img src={posterImage} alt={name} width="280" height="175" />
       </div>
