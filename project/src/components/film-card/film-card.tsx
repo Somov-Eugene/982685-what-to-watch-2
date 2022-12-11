a1ca2ca901238d9ac774b15ca39820e876fa5c45
Somov Eugene <somov.eugene.2012@gmail.com>
2022-12-11 21:12:51 +0300

    Обновляет компонент «Карточка фильма».

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,16 +1,21 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import { FilmType } from '../../types/films';
+
 type FilmCardProps = {
-  name: string;
-  posterImage: string;
+  film: FilmType;
 }
 
-function FilmCard({ name, posterImage }: FilmCardProps): JSX.Element {
+function FilmCard({ film }: FilmCardProps): JSX.Element {
+  const {id, name, posterImage} = film;
+
   return (
     <article className="small-film-card catalog__films-card">
       <div className="small-film-card__image">
         <img src={posterImage} alt={name} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href="film-page.html">{name}</a>
+        <Link className="small-film-card__link" to={`${AppRoute.Film}/${id}`}>{name}</Link>
       </h3>
     </article>
   );
