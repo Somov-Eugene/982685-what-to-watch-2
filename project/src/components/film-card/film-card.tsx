3d6f68c1a3bbc8935c7f9a51e3b681942b1d52f5
Somov Eugene <somov.eugene.2012@gmail.com>
2022-12-12 23:02:07 +0300

    Подключает «Видеоплеер» к карточке с фильмом.

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,24 +1,32 @@
 import { Link } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import VideoPlayer from '../video-player/video-player';
 import { FilmType } from '../../types/films';
+import { AppRoute } from '../../const';
 
 type FilmCardProps = {
   film: FilmType;
+  isRunPreview: boolean;
   handleMouseEnter: () => void;
   handleMouseLeave: () => void;
+  handleFilmCardClick: () => void;
 }
 
-function FilmCard({ film, handleMouseEnter, handleMouseLeave }: FilmCardProps): JSX.Element {
-  const {id, name, posterImage} = film;
+function FilmCard({ film, isRunPreview, handleMouseEnter, handleMouseLeave, handleFilmCardClick }: FilmCardProps): JSX.Element {
+  const { id, name, posterImage } = film;
 
   return (
     <article
       className="small-film-card catalog__films-card"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
+      onClick={handleFilmCardClick}
     >
       <div className="small-film-card__image">
-        <img src={posterImage} alt={name} width="280" height="175" />
+        {
+          isRunPreview
+            ? <VideoPlayer film={film} />
+            : <img src={posterImage} alt={name} width="280" height="175" />
+        }
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`${AppRoute.Film}/${id}`}>{name}</Link>
