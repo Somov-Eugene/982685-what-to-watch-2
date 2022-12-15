6f79926f3b2fc926e39cde3358e98bbdf990b107
Somov Eugene <somov.eugene.2012@gmail.com>
2022-12-15 22:17:41 +0300

    Подключает компонент «Табы» к компоненту страницы «MoviePage».

--- project/src/pages/movie-page/movie-page.tsx
+++ project/src/pages/movie-page/movie-page.tsx
@@ -1,4 +1,23 @@
-function MoviePage(): JSX.Element {
+import { useParams } from 'react-router-dom';
+import FilmTabs from '../../components/film-tabs/film-tabs';
+import NotFoundPage from '../not-found-page/not-found-page';
+import { FilmsType } from '../../types/films';
+import { ReviewsType } from '../../types/reviews';
+
+type MoviePageProps = {
+  films: FilmsType;
+  reviews: ReviewsType;
+}
+
+function MoviePage({ films, reviews } :MoviePageProps): JSX.Element {
+  const { id } = useParams();
+
+  const currentFilm = films.find((film) => `${film.id}` === id);
+
+  if (!currentFilm) {
+    return <NotFoundPage />;
+  }
+
   return (
     <>
       <section className="film-card film-card--full">
@@ -64,39 +83,7 @@ function MoviePage(): JSX.Element {
               <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
             </div>
 
-            <div className="film-card__desc">
-              <nav className="film-nav film-card__nav">
-                <ul className="film-nav__list">
-                  <li className="film-nav__item film-nav__item--active">
-                    <a className="film-nav__link" href="#todo">Overview</a>
-                  </li>
-                  <li className="film-nav__item">
-                    <a className="film-nav__link" href="#todo">Details</a>
-                  </li>
-                  <li className="film-nav__item">
-                    <a className="film-nav__link" href="#todo">Reviews</a>
-                  </li>
-                </ul>
-              </nav>
-
-              <div className="film-rating">
-                <div className="film-rating__score">8,9</div>
-                <p className="film-rating__meta">
-                  <span className="film-rating__level">Very good</span>
-                  <span className="film-rating__count">240 ratings</span>
-                </p>
-              </div>
-
-              <div className="film-card__text">
-                <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave&rsquo;s friend and protege.</p>
-
-                <p>Gustave prides himself on providing first-className service to the hotel&rsquo;s guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave&rsquo;s lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.</p>
-
-                <p className="film-card__director"><strong>Director: Wes Anderson</strong></p>
-
-                <p className="film-card__starring"><strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other</strong></p>
-              </div>
-            </div>
+            <FilmTabs film={currentFilm} reviews={reviews} />
           </div>
         </div>
       </section>
