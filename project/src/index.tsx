8e7e32178ce833a0a992d44febe09b7e5f78d804
Somov Eugene <somov.eugene.2012@gmail.com>
2022-12-18 21:09:21 +0300

    Подключает хранилище.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
+import { store } from './store/index';
 import { mockPromo, mockFilms } from './mocks/films';
 import { mockReviews } from './mocks/reviews';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -10,6 +13,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App promo={mockPromo} films={mockFilms} reviews={mockReviews} />
+    <Provider store={store}>
+      <App promo={mockPromo} films={mockFilms} reviews={mockReviews} />
+    </Provider>
   </React.StrictMode>
 );
