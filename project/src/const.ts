f1972a36c59a0d6943e983f4401906b55f3fb7f9
Somov Eugene <somov.eugene.2012@gmail.com>
2022-12-18 21:08:14 +0300

    Добавляет функции для получения списка жанров и списка фильмов определённого жанра.

--- project/src/const.ts
+++ project/src/const.ts
@@ -48,6 +48,8 @@ export const FilmScores: {
   },
 ];
 
+export const QTY_DISPLAYED_FILMS_DEFAULT = 8;
+export const QTY_GENRES_MAX = 9;
 export const PLAYER_DELAY = 1000;
 export const MINUTES_IN_HOUR = 60;
 
@@ -55,4 +57,20 @@ export enum FilmTab {
   Overview = 'Overview',
   Details = 'Details',
   Reviews = 'Reviews',
+}
+
+export enum Genres {
+  AllGenres = 'All genres',
+  Action = 'Action',
+  Adventure = 'Adventure',
+  Comedie = 'Comedie',
+  Crime = 'Crime',
+  Documentary = 'Documentary',
+  Drama = 'Drama',
+  Fantasy = 'Fantasy',
+  Horror = 'Horror',
+  KidsAndFamily = 'Kids & Family',
+  Romance = 'Romance',
+  SciFi = 'Sci-Fi',
+  Thriller = 'Thriller',
 }
--- project/src/utils/utils.ts
+++ project/src/utils/utils.ts
@@ -1,4 +1,5 @@
-import { FilmScores } from '../const';
+import { FilmsType } from '../types/films';
+import { Genres, FilmScores, QTY_GENRES_MAX } from '../const';
 
 export const filmRatingLevel = (rank: number): string => {
   let result = 'Unknown';
@@ -35,4 +36,20 @@ export const starringList = (actors: string[], separator: string): string => {
 export const reviewDateFormat = (date: string): string => {
   const dt = new Date(date);
   return new Intl.DateTimeFormat('en-US', { dateStyle: 'long' }).format(dt); // December 24, 2022
+};
+
+export const getListGenres = (films: FilmsType): Genres[] => {
+  const UniqueGenres = new Set<Genres>();
+  films.forEach(({ genre }) => UniqueGenres.add(genre as Genres));
+
+  // В списке жанров отображается не больше 9 жанров + All genres.
+  return [Genres.AllGenres, ...Array.from(UniqueGenres).sort().slice(0, QTY_GENRES_MAX)];
+};
+
+export const filteredFilmsByGenre = (films: FilmsType, genre: Genres): FilmsType => {
+  if (genre === Genres.AllGenres) {
+    return films;
+  } else {
+    return films.filter((film) => (film.genre === genre));
+  }
 };
